8c50f26d105407f8f17bc66c21d6b784b484978c
Esperanzq <46347747+Esperanzq@users.noreply.github.com>
2019-01-10 14:27:28 +0300

    Update final_logger.py

--- final_logger.py
+++ final_logger.py
@@ -1,16 +1,13 @@
 import logging
 
+logger = logging.getLogger(__name__)
+logfile = "Coffee_for_me.log"
 
-def final_logger():
-    logger = logging.getLogger(__name__)
-    logfile = "CoffeeForMe.log"
+formatter = logging.Formatter('%(asctime)s - %(name)s:  %(levelname)s - %(message)s')
 
-    formatter = logging.Formatter('%(asctime)s - %(name)s : %(levelname)s - %(message)s')
+file_handler = logging.FileHandler(logfile)
+file_handler.setLevel(logging.DEBUG)
+file_handler.setFormatter(formatter)
 
-    file_handler = logging.FileHandler(logfile)
-    file_handler.setLevel(logging.INFO)
-    file_handler.setFormatter(formatter)
-
-    logger.addHandler(file_handler)
-
-    return logger
+logger.addHandler(file_handler)
+logger.setLevel(logging.DEBUG)
