cb9dead78fe9dc8e73aee520d66ebb7c4452b73e
Esperanzq <46347747+Esperanzq@users.noreply.github.com>
2019-01-17 20:02:03 +0300

    Update final_logger.py

--- final_logger.py
+++ final_logger.py
@@ -5,13 +5,8 @@ import logging
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
-path = os.path.abspath('./log')
-try:
-    os.mkdir(path)
-except OSError:
-    pass
 
-file_handler = logging.handlers.RotatingFileHandler(os.path.abspath('./log/Coffee_for_me_log'), maxBytes=1048576, backupCount=2)
+file_handler = logging.handlers.RotatingFileHandler(os.path.abspath('./Coffee_for_me_log'))
 formatter = logging.Formatter(
     "%(asctime)s : func - %(funcName)-16s - %(lineno)-3s line : %(filename)-16s : %(levelname)-6s : %(message)s")
 file_handler.setFormatter(formatter)
